ec90fe42972524ee2a95d8462758352456a19cb9
burguer <burguer@Sergios-MacBook-Pro.local>
2017-02-12 17:33:46 -0800

    Data core class added

---
+++ lib/borderbot/dcore.rb
@@ -0,0 +1,13 @@
+# Welcome to Dcore( Data Sorting Core )
+# Here is where all data is sorted.
+
+class Dcore
+  attr_accessor :ports
+  def initialize
+      @ports = []
+  end
+
+  def first_method
+    return @ports
+  end
+end
