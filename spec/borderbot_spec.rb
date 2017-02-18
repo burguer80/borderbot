8469d8c9f2ba8e5b0683b12ad8e4a377a7f4b101
burguer <burguer@Sergios-MacBook-Pro.local>
2017-02-17 20:47:14 -0800

    test cleaned

--- spec/borderbot_spec.rb
+++ spec/borderbot_spec.rb
@@ -5,11 +5,5 @@ describe Borderbot do
     expect(Borderbot::VERSION).not_to be nil
   end
 
-  it "Agent instance created succesfully" do
-    agent = Agent.new
-    expect(agent.class).to eq(Agent)
-    expect(agent.bwt_url.class).to eq(String)
-    expect(agent.ports.class).to eq(Array)
-  end
 
 end
